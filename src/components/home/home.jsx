57d5fc2391a98eb757b72ed7f4011e130d889e35
Giovani Araujo <giovanijesus1997@gmail.com>
2025-02-15 02:16:38 -0300

    feat: screen home up

---
+++ src/components/home/home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div className='home'>
+        <div className='container-home'>
+            <div className='new1'>
+                <h2></h2>
+            </div>
+            <div className='new2'>
+                <h3>Manutenção</h3>
+            </div>
+            <div className='new3'>
+
+            </div>
+            <div className='new4'>
+
+            </div>
+            <div className='new5'>
+
+            </div>
+            <div className='new6'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default home
